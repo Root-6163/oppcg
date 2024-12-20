6d22e2a0d5796362d7bff3581a74c33504893826
Root <sweetrat6163@gmail.com>
2024-12-20 10:44:29 +0530

    Update 71.cpp
    correction map declaration and
    if else statement (iter1= populationmap.end()

--- 71.cpp
+++ 71.cpp
@@ -1,27 +1,39 @@
-//input code (1):
 #include <iostream>
 #include <map>
 #include <string>
 using namespace std;
-int main(){
 
-typedef map<string,int>maptype;
-maptype populationmap
-populationmap.insert(pair<string,int>("maharashtra :",709387));
-populationmap.insert(pair<string,int>("Rajastan : ",298374));
-populationmap.insert(pair<string,int>("kashmir :",234546));
-populationmap.insert(pair<string,int>("punjab :",938472));
+int main() {
+    // Define the map type and create the population map
+    typedef map<string, int> maptype;
+    maptype populationmap;
 
-maptype::iterator iter;
-cout<<"size of populatoin map"<<populationmap.size()<<endl;
-string state_name;
-cin>>state_name;
-iter = populationmap.find(state_name);
+    // Insert values into the population map
+    populationmap.insert(pair<string, int>("Maharashtra", 709387));
+    populationmap.insert(pair<string, int>("Rajasthan", 298374));
+    populationmap.insert(pair<string, int>("Kashmir", 234546));
+    populationmap.insert(pair<string, int>("Punjab", 938472));
 
-if iter != populationmap.end(){
-cout<<state_name<<" populatoin is"<<iter -> second;
-else
-cout<<"key is not populationmap"<<"\n";
-population.clear();
+    // Display the size of the population map
+    cout << "Size of population map: " << populationmap.size() << endl;
+
+    // Prompt the user to search for a state
+    string state_name;
+    cout << "Enter the state name to search: ";
+    cin >> state_name;
+
+    // Find the state in the map
+    maptype::iterator iter = populationmap.find(state_name);
+
+    if (iter != populationmap.end()) {
+        // If found, display the population
+        cout << state_name << " population is " << iter->second << endl;
+    } else {
+        // If not found, display a message
+        cout << "Key is not in the population map." << endl;
+    }
+
+    // Clear the map
+    populationmap.clear();
+    return 0;
 }
-
